1a3206fb91913ad49c305a2f7f7b5e28cec5854d
Ishihara Yoshihisa <pokkuru4413@gmail.com>
2021-01-22 14:48:10 +0900

    ディジタル制御

--- led/led.ino
+++ led/led.ino
@@ -1,12 +1,12 @@
 void setup()
 {
-  pinMode(2, OUTPUT);
+  pinMode(13, OUTPUT);
+  digitalWrite(13, HIGH);
+  delay(1000);
+  digitalWrite(13, LOW);
+  delay(1000);
 }
 
 void loop()
 {
-   digitalWrite(2, HIGH);
-   delay(1000);
-   digitalWrite(2, LOW);
-   delay(1000);
 }
---
+++ makura/makura.ino
@@ -0,0 +1,13 @@
+// ギア付きモータの制御
+int i;
+void setup()
+{
+  pinMode(7, OUTPUT);
+  digitalWrite(7, HIGH);
+  delay(1000);
+  analogWrite(7, LOW);
+}
+
+void loop(){
+
+}
