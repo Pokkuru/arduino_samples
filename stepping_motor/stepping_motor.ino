fb70e3929f984237596989cb394ea46afaae3e57
Ishihara Yoshihisa <pokkuru4413@gmail.com>
2021-02-07 19:42:48 +0900

    クロックどうやって出すの？

--- stepping_motor/stepping_motor.ino
+++ stepping_motor/stepping_motor.ino
@@ -1,15 +1,16 @@
-#include <Stepper.h>
-const float turnSteps = 200;
-float rpm = 30;
-float Steps = 1; 
- 
-Stepper myStepper(turnSteps, 8,9,10,11);
- 
 void setup() {
-  myStepper.setSpeed(rpm) ;
+  pinMode(8, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
+  digitalWrite(8, LOW);
+  digitalWrite(9, LOW);
+  digitalWrite(10, HIGH);
+  pinMode(11, OUTPUT);
 }
  
 void loop() {
-  myStepper.step(3);
-  delay(1000);
+ digitalWrite(11, HIGH);
+ delay(15);
+ digitalWrite(11, LOW);
+ delay(15);
 }
