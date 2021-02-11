d41c053bcaeb703265b925900e6e4054bf94a8fa
Ishihara Yoshihisa <pokkuru4413@gmail.com>
2021-02-12 00:10:23 +0900

    方向制御が効かない

--- stepping_motor/stepping_motor.ino
+++ stepping_motor/stepping_motor.ino
@@ -1,15 +1,24 @@
-#include <Stepper.h>
-const float turnSteps = 200;
-float rpm = 30;
-float Steps = 1; 
- 
-Stepper myStepper(turnSteps, 8,9,10,11);
- 
 void setup() {
-  myStepper.setSpeed(rpm) ;
+  pinMode(2, OUTPUT);
+  pinMode(8, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
+  digitalWrite(8, LOW);
+  digitalWrite(9, LOW);
+  digitalWrite(10, HIGH);
+  digitalWrite(7, HIGH);
+  delay(20);
 }
  
 void loop() {
-  myStepper.step(3);
-  delay(1000);
+  tone(6, 850); //普通に動く
+  delay(200);
+  digitalWrite(2, HIGH);
+  noTone(); // 2/13に試す。駄目なら消す
+  delay(800);
+  //digitalWrite(7, HIGH);
+  //delay(20);
+  digitalWrite(2, LOW);
 }
