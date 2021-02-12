927f060e75366c84664c4930f775a5a262545421
Ishihara Yoshihisa <pokkuru4413@gmail.com>
2021-02-12 13:07:07 +0900

    toneでもHIGH、LOW切り替えでも良い
    モータドライバへのピンの挿し間違え注意
    周波数は周期からしっかりと計算することHIGHとLOWの一回で1周期

--- stepping_motor/stepping_motor.ino
+++ stepping_motor/stepping_motor.ino
@@ -1,24 +1,34 @@
 void setup() {
   pinMode(2, OUTPUT);
+  pinMode(7, OUTPUT);
   pinMode(8, OUTPUT);
   pinMode(9, OUTPUT);
   pinMode(10, OUTPUT);
   pinMode(6, OUTPUT);
-  pinMode(7, OUTPUT);
   digitalWrite(8, LOW);
   digitalWrite(9, LOW);
   digitalWrite(10, HIGH);
-  digitalWrite(7, HIGH);
-  delay(20);
+  digitalWrite(7, LOW);
 }
  
 void loop() {
-  tone(6, 850); //普通に動く
-  delay(200);
-  digitalWrite(2, HIGH);
-  noTone(); // 2/13に試す。駄目なら消す
-  delay(800);
+  /*
+  tone(6, 880); //普通に動く
+  delay(500);
+  noTone(6); // 2/13に試す。駄目なら消す
+  delay(1000);
+  */
+  //digitalWrite(7, LOW);
+  //delay(20);
+  // 6ピンをHIGH, LOW切り替え
+  for(int i = 0; i < 1000; i++){
+    digitalWrite(2, HIGH);
+    digitalWrite(6, HIGH);
+    delayMicroseconds(720);
+    digitalWrite(6, LOW);
+    delayMicroseconds(720);
+    
+  }
   //digitalWrite(7, HIGH);
   //delay(20);
-  digitalWrite(2, LOW);
 }
